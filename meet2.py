78d24c906dc34e1a714b65ee25e5af457402ad8d
amarmuhammadasri_Alprog-C <onlywith.amar@gmail.com>
2025-11-06 17:06:49 +0800

    Add files via upload

---
+++ meet2.py
@@ -0,0 +1,30 @@
+#Variable adalah cara menyimpan nilai atau value (aljabar)
+#x: variable =: assigment 13: nilai (x=3)
+# seperti
+x = 20
+y = 30
+lebar = 40
+print("niali x adalah :", x)
+print("niali y adalah :", y)
+print("niali lebar adalah :", lebar)
+# ATURAN PENAMAAN VARIABLE
+# 1 tidak boleh diawali angka
+latoto99 = 123
+print("nilai dari SKATER99 adalah :", latoto99)
+# 2 jika lebih dari satu kata, gabung dengan_
+Amee_GG = 456
+print("niali dari Amee_GG :", Amee_GG)
+# 3 bisa juga menggabungkan dengan huruf kpital
+AmeeGg = 789
+print("niali dari AmeeGg :", AmeeGg)
+# PEMENGGALAN INDERECT
+x = 111
+z = x
+print("niali dari z :",z)
+# tugas
+# nama saya adalah = (nama anda)
+# nim saya adalah = (nim anda)
+a = "Amar muhammad asri"
+b = 25241089
+print("nama saya adalah :", a)
+print("nim saya adalah :", b)
