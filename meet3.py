5bbd21be7764da296b320c133eff1a67b7961cd8
amarmuhammadasri_Alprog-C <onlywith.amar@gmail.com>
2025-11-13 17:33:05 +0800

    Add files via upload

---
+++ meet3.py
@@ -0,0 +1,17 @@
+# type data = dari sebuah nilai 
+
+x = 10 # type data integer 
+print("type data dari x", type(x))
+
+
+y = 3.5 # type data float 
+print("nilai dari y =", y)
+print("type data dari y =",type (y))
+
+nama = "ame" # type data string
+print("nilai dari nama =", nama)
+print("type data dari nama =", type(nama)) 
+
+kebenaran = True # type data bolean 
+print("nilai dari kebenaran  =", kebenaran)
+print("type dari kebenaran  =", (kebenaran))
