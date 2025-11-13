6e37522e6b729c79a41e0c55091ecc85ea5dd956
amarmuhammadasri_Alprog-C <onlywith.amar@gmail.com>
2025-11-13 17:33:52 +0800

    Add files via upload

---
+++ meet4.py
@@ -0,0 +1,21 @@
+# mengambil infut dari user
+
+nama = input("masukkan nama anda : ")
+print("selamat datang", nama)
+print("nama adalah", nama)
+print(type (nama))
+
+bil_int = int(input("masukkan umur anda :"))
+print("bilangan anda adalah", bil_int)
+print("nilai dari bil_float", bil_int)
+print(type(bil_int))
+
+bil_float = float(input("masukkkan umur anda:"))
+print("bilangan anda adalah", bil_float)
+print("nilai dari bil_float", bil_float)
+print(type(bil_float))
+
+bil_bool = bool(input("masukkan umur anda :"))
+print("bilangan anda adalah", bil_bool)
+print("nilai dari bil_float", bil_bool)
+print(type(bil_bool))
