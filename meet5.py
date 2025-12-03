673d7f4c5d429eb78680a60ee831576714951ef7
amarmuhammadasri_Alprog-C <onlywith.amar@gmail.com>
2025-12-03 17:42:43 +0800

    Add files via upload

---
+++ meet5.py
@@ -0,0 +1,51 @@
+# operasi aritmatika
+# + - x /
+# penjumlahan (+)
+# pengurangan (-)
+# perkalian (*)
+# pembagian (/)
+# perpangkatan (**)
+# modulus (%)
+# floor division (//)
+
+# penjumlahan 
+a = 6
+b = 4
+nilai = a + b
+print(a, "+", b, "=", nilai)
+
+# pengurangan 
+a = 10
+b = 9
+nilai = a - b
+print(a, "-", b, "=", nilai)
+
+# perkalian
+a = 2
+b = 2
+nilai = a * b
+print(a, "x", b, "=", nilai)
+
+# pembagian
+a = 15
+b = 5
+nilai = a / b
+print(a, ":", b, "=", nilai)
+
+# perpangkatan 
+a = 3
+b = 1
+nilai = a ** b
+print(a, "^", b, "=", nilai)
+
+# modulus
+a = 32
+b = 5
+nilai = a % b
+print(a, "%", b, "=", nilai)
+
+# floor division // pembulatan
+a = 25
+b = 5
+nilai = a // b
+print(a, "//", b, "=", nilai)
